4c6aeda59fe1358c80cf48ff659210d798cdcd94
alma-mata <mat23038@uvg.edu.gt>
2025-04-24 21:35:21 -0600

    Creación del proyecto
    Creación de los archivos necesarios para hacer el Laboratorio6

---
+++ Laboratorio6/Laboratorio6/main.c
@@ -0,0 +1,34 @@
+/*
+ * Universidad del Valle de Guatemala
+ * IE2023: Programacion de Microcontroladores
+ * Laboratorio6.c
+ * Autor: Alma Lisbeth Mata Ixcayau
+ * Proyecto: Laboratorio 6
+ * Descripcion: Control de servomotores y led con PWM
+ * Creado: 24/04/2025
+*/
+/****************************************/
+// Encabezado
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <stdint.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+/****************************************/
+// Prototipos de función
+
+/****************************************/
+// Función principal
+int main(void) {
+	
+	while (1) {
+	}
+}
+
+/****************************************/
+// Subrutinas sin Interrupcion
+
+
+/****************************************/
+// Subrutinas de Interrupcion
