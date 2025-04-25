ac3fcdc8404a1416a86e2fc5e737d8dfe52853d7
alma-mata <mat23038@uvg.edu.gt>
2025-04-24 23:39:54 -0600

    Pre laboratorio
    Se cumplen con los dos requisitos del Pre laboratorio:
    1. Se muestra un carácter en la terminal
    2. Se muestra el código ASCII con leds en el puerto B y dos pines del puerto D

--- Laboratorio6/Laboratorio6/main.c
+++ Laboratorio6/Laboratorio6/main.c
@@ -17,18 +17,61 @@
 
 /****************************************/
 // Prototipos de función
+void setup(void);
+void UART_init(void);
+void send_CHAR(unsigned char caracter);
+void send_STRING(char* texto);
 
+volatile uint8_t received_RX = 0;
 /****************************************/
 // Función principal
 int main(void) {
 	
+	setup(); // Configuraciones iniciales
+	//send_STRING("Hola\r\n");
 	while (1) {
+		send_CHAR('H');
+		_delay_ms(1000);
 	}
 }
 
 /****************************************/
 // Subrutinas sin Interrupcion
+void setup(void){
+	cli();
+	
+	// Entradas y Salidas
+	DDRD |= (1 << DDD7) | (1 << DDD6); 
+	DDRB = 0xFF; // Todo PORTB como salida
+	
+	UART_init(); // Configuracion del UART
+	
+	sei();
+}
 
+void UART_init(void){
+	UBRR0 = 103;
+	
+	UCSR0B = (1 << RXCIE0) | (1 << RXEN0) | (1 << TXEN0); //Rx int. | Rxen | Txen
+	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); //Async. | No parity | 1 stop | 8 data
+}
 
+void send_CHAR(unsigned char caracter){
+	while (!(UCSR0A & (1 << UDRE0))); //Revisar si UDR está vacío
+	UDR0 = caracter;
+}
+
+void send_STRING(char* texto){
+	uint8_t i = 0;
+	while (texto[i] != '\0'){
+		send_CHAR(texto[i]);
+		i++;
+	}
+}
 /****************************************/
 // Subrutinas de Interrupcion
+ISR(USART_RX_vect){
+	received_RX = UDR0;
+	PORTB = ((received_RX & 0xFC) >> 2);
+	PORTD = (PORTD & 0x3F) | ((received_RX & 0x03) << 6);
+}
